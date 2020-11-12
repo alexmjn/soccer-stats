d8dc67aeb8a7578dfb43b12e1470d3e8848f3342
alexmjn <a.jenkneary@gmail.com>
2020-11-12 16:20:21 -0500

    initial files

---
+++ sample_kloppy.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+
+from kloppy import datasets, transform, to_pandas
+from kloppy.utils import performance_logging
+
+
+def main():
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    logger = logging.getLogger(__name__)
+
+    dataset = datasets.load("statsbomb", {"event_types": ["pass", "take_on","carry","shot"]}
+    )
+
+    with performance_logging("transform", logger=logger):
+        dataset = transform(
+            dataset,
+            to_orientation="FIXED_HOME_AWAY",
+            to_pitch_dimensions=[(-5500, 5500), (-3300, 3300)]
+        )
+
+    with performance_logging("to pandas", logger=logger):
+        dataframe = to_pandas(dataset)
+
+    print(dataframe[:50].to_string())
+
+
+if __name__ == "__main__":
+    main()
---
+++ transfer_markt.py
@@ -0,0 +1,4 @@
+# functions to efficiently populate transfermarkt data into csv files
+# in order to transfer into database.
+# idea is to create different database sheets to represent each entity.
+# sqlite?
